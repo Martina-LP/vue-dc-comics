acf614f1d1c9522f4304c75ea6769ed22238c233
Martina-LP <madlp8@gmail.com>
2022-01-14 10:06:57 +0100

    Icons

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,7 +1,28 @@
 <template>
   <section>
     <div class="container">
-      Icons Icons Icons
+      <div class="row">
+        <div class="card">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="Comics">
+          <span>DIGITAL COMICS</span>
+        </div>
+        <div class="card">
+          <img src="../assets/img/buy-comics-merchandise.png" alt="Merchandise">
+          <span>DC MERCHANDISE</span>
+        </div>
+        <div class="card">
+          <img src="../assets/img/buy-comics-subscriptions.png" alt="Subscriptions">
+          <span>SUBSCRIPTION</span>
+        </div>
+        <div class="card">
+          <img src="../assets/img/buy-comics-shop-locator.png" alt="Shop Locator">
+        <span>COMIC SHOP LOCATOR</span>
+        </div>
+        <div class="card">
+          <img src="../assets/img/buy-dc-power-visa.svg" alt="Power Visa">
+          <span>DC POWER VISA</span>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -17,6 +38,32 @@ export default {
 
 section {
   background-color: $primary_color;
+  height: 150px;
+
+  .row {
+    display: flex;
+    height: 120px;
+    padding: 0 16px;
+    justify-content: space-between;
+    align-items: center;
+  
+    .card {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
+      cursor: pointer;
+
+      img {
+        width: 40px;
+        margin: 10px;
+      }
+
+      span {
+        font-size: 15px;
+      }
+    }
+  } 
 }
 
 </style>
